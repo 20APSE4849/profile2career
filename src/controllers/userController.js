d07c51016dcafd6ec7d5c35fc528ac692c326537
Ravindu_Chandrarathna <116499993+20APSE4849@users.noreply.github.com>
2024-06-01 19:17:20 +0530

    Initial commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+    res.status(200).json({ message: 'Login successful' });
+};
